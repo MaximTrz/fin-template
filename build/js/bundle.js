a6ffc395c82804ba14746da3ee64fb436e672d3e
Maxim <maxim.trz@gmail.com>
2024-05-14 16:03:23 +0500

    first commit

---
+++ build/js/bundle.js
@@ -0,0 +1,5 @@
+/*
+/ /= ../../node_modules/jquery/dist/jquery.js
+ */
+"use strict";
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUZBIiwiZmlsZSI6ImJ1bmRsZS5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8qXHJcbi8gLz0gLi4vLi4vbm9kZV9tb2R1bGVzL2pxdWVyeS9kaXN0L2pxdWVyeS5qc1xyXG4gKi8iXX0=
